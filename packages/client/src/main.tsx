5e6339e6b3e349eb1ba655748d42ef002f942b28
Elena Ivanova <x.ElenaI@yandex.ru>
2025-08-22 22:25:37 +0700

    fix: fixed error in console about styles in server and client

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -7,14 +7,17 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { routes } from './routes'
 import { ThemeProvider } from '@gravity-ui/uikit'
+import { StyleSheetManager } from 'styled-components'
 
 const router = createBrowserRouter(routes)
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
-  <Provider store={store}>
-    <ThemeProvider theme="light">
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </Provider>
+  <StyleSheetManager>
+    <Provider store={store}>
+      <ThemeProvider theme="light">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </StyleSheetManager>
 )
