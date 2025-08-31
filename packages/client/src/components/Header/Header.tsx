9c0a8f55846e494ad3b3fffb56b12d3d8f47e05e
Elena Ivanova <x.ElenaI@yandex.ru>
2025-08-31 14:39:05 +0700

    feat: added main page

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -6,23 +6,26 @@ type MenuItem = {
   link: string
 }
 
-const MENU_ITEMS: MenuItem[] = [
-  { text: 'Home', link: '/' },
-  { text: 'Leaderboard', link: '/leaderboard' },
-  { text: 'Forum', link: '/forum' },
-  { text: 'Profile', link: '/profile' },
-  { text: 'Start', link: '/start' },
-]
-
 export const Header = () => {
+  const menuItems: MenuItem[] = [
+    { text: 'Home', link: '/' },
+    { text: 'Leaderboard', link: '/leaderboard' },
+    { text: 'Forum', link: '/forum' },
+    { text: 'Profile', link: '/profile' },
+    { text: 'Start', link: '/start' },
+    { text: 'mainPage', link: '/mainPage' },
+  ]
+
   return (
     <div className={s['header']}>
       <div className={s['header__logo']}>CROCODILE</div>
       <nav className={s['header__menu']}>
         <ul className={s['header__links-list']}>
-          {MENU_ITEMS.map((item: MenuItem, index: number) => {
+          {menuItems.map((item: MenuItem, index: number) => {
             return (
-              <li key={index} className={s['header__links-list-item']}>
+              <li
+                key={'item_' + item.text + '_' + index}
+                className={s['header__links-list-item']}>
                 <Link className={s['header__links-list-link']} to={item.link}>
                   {item.text}
                 </Link>
