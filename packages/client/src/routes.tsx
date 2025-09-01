66d02c320d66830bb77f9c6fdeb5561aac9599e7
Dmitriy Solovev <dmptts@gmail.com>
2025-09-01 18:17:46 +0300

    feat :sparkles: adds forum pages

--- packages/client/src/routes.tsx
+++ packages/client/src/routes.tsx
@@ -1,15 +1,15 @@
-import { AppDispatch, RootState } from './store'
-
+import { ForumPage, initForumPage } from './pages/Forum/Forum'
+import { GamePage, initGamePage } from './pages/Game'
+import { initLeaderboardPage, LeaderboardPage } from './pages/Leaderboard'
 import { initMainPage, MainPage } from './pages/Main'
+import { initNotFoundPage, NotFoundPage } from './pages/NotFound'
+import { initProfilePage, ProfilePage } from './pages/Profile'
 import { initSignInPage, SignInPage } from './pages/SignIn'
 import { initSignUpPage, SignUpPage } from './pages/SignUp'
-import { initProfilePage, ProfilePage } from './pages/Profile'
-import { initGamePage, GamePage } from './pages/Game'
-import { initLeaderboardPage, LeaderboardPage } from './pages/Leaderboard'
-import { initForumPage, ForumPage } from './pages/Forum'
-import { initTopicPage, TopicPage } from './pages/Topic'
-import { initNotFoundPage, NotFoundPage } from './pages/NotFound'
 import { StartPage } from './pages/StartPage'
+import { initSubmitTopicPage, SubmitTopicPage } from './pages/SubmitTopic'
+import { initTopicPage, TopicPage } from './pages/Topic'
+import { AppDispatch, RootState } from './store'
 
 export type PageInitContext = {
   clientToken?: string
@@ -56,6 +56,11 @@ export const routes = [
     path: '/forum',
     Component: ForumPage,
     fetchData: initForumPage,
+  },
+  {
+    path: '/forum/submit',
+    Component: SubmitTopicPage,
+    fetchData: initSubmitTopicPage,
   },
   {
     path: '/forum/:topicId',
