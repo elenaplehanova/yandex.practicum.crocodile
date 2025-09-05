147fa03ab2d2188b4f8d28f8b8cb5e742fb2a5fd
Elena Ivanova <x.ElenaI@yandex.ru>
2025-09-05 09:12:09 +0700

    refactor: code review
    - the chatset meta tag has been added to index.html
    - menu items is placed above the component as a constant
    - the menu item key has been changed to the index

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -6,26 +6,23 @@ type MenuItem = {
   link: string
 }
 
-export const Header = () => {
-  const menuItems: MenuItem[] = [
-    { text: 'Home', link: '/' },
-    { text: 'Leaderboard', link: '/leaderboard' },
-    { text: 'Forum', link: '/forum' },
-    { text: 'Profile', link: '/profile' },
-    { text: 'Start', link: '/start' },
-    { text: 'mainPage', link: '/mainPage' },
-  ]
+const MENU_ITEMS: MenuItem[] = [
+  { text: 'Home', link: '/' },
+  { text: 'Leaderboard', link: '/leaderboard' },
+  { text: 'Forum', link: '/forum' },
+  { text: 'Profile', link: '/profile' },
+  { text: 'Start', link: '/start' },
+]
 
+export const Header = () => {
   return (
     <div className={s['header']}>
       <div className={s['header__logo']}>CROCODILE</div>
       <nav className={s['header__menu']}>
         <ul className={s['header__links-list']}>
-          {menuItems.map((item: MenuItem, index: number) => {
+          {MENU_ITEMS.map((item: MenuItem, index: number) => {
             return (
-              <li
-                key={'item_' + item.text + '_' + index}
-                className={s['header__links-list-item']}>
+              <li key={index} className={s['header__links-list-item']}>
                 <Link className={s['header__links-list-link']} to={item.link}>
                   {item.text}
                 </Link>
