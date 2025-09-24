46ae32d108a936ab85979a768ef4619d74b97966
Elena Ivanova <x.ElenaI@yandex.ru>
2025-09-24 12:27:28 +0700

    feat: added background page and header for Profile Page and Forum Page

--- packages/client/src/pages/SubmitTopic/SubmitTopic.tsx
+++ packages/client/src/pages/SubmitTopic/SubmitTopic.tsx
@@ -3,30 +3,43 @@ import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { withAuth } from '../../hocs/withAuth'
 import styles from './SubmitTopic.module.scss'
+import { Header } from '@components/Header'
+import { Helmet } from 'react-helmet'
 
 export const SubmitTopicPage: FC = withAuth(() => {
   const navigate = useNavigate()
 
   return (
-    <Flex
-      className={styles['root']}
-      direction="column"
-      justifyContent="center"
-      alignItems="center">
-      <form className={styles['form']}>
-        <TextInput label="Заголовок" size="xl" />
-        <TextArea rows={5} size="xl" placeholder="Введите сообщение" />
-        <Flex gap="2" justifyContent="flex-end">
-          <Button onClick={() => navigate('/forum')}>Отмена</Button>
-          <Button
-            type="submit"
-            view="action"
-            onClick={() => navigate('/forum/1')}>
-            Отправить
-          </Button>
-        </Flex>
-      </form>
-    </Flex>
+    <div className={styles['wrapper']}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Submit</title>
+        <meta
+          name="description"
+          content="Страница создания новой темы форума"
+        />
+      </Helmet>
+      <Header />
+      <Flex
+        className={styles['root']}
+        direction="column"
+        justifyContent="center"
+        alignItems="center">
+        <form className={styles['form']}>
+          <TextInput label="Заголовок" size="xl" />
+          <TextArea rows={5} size="xl" placeholder="Введите сообщение" />
+          <Flex gap="2" justifyContent="flex-end">
+            <Button onClick={() => navigate('/forum')}>Отмена</Button>
+            <Button
+              type="submit"
+              view="action"
+              onClick={() => navigate('/forum/1')}>
+              Отправить
+            </Button>
+          </Flex>
+        </form>
+      </Flex>
+    </div>
   )
 })
 
